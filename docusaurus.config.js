7985b2cf6a9ba0054e2e3d8263da144520bf02cf
Stavis <83443850+stavis-dev@users.noreply.github.com>
2023-07-27 17:04:51 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,6 +66,11 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      docs: {
+          sidebar: {
+            hideable: true,
+            autoCollapseCategories: true,
+          },
       navbar: {
         title: 'Главная',
         logo: {
