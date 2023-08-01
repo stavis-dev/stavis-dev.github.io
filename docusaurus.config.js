026dc74b2e92f3c2b5e5a2b4d5b072eea7cc01d9
Stavis <83443850+stavis-dev@users.noreply.github.com>
2023-08-01 23:30:47 +0300

    remuve

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,12 +126,6 @@ const config = {
             position: 'left',
             label: 'Python',
           },
-          {
-            to: 'category/python/readme',
-            label: 'Python2',
-            position: 'left',
-            activeBaseRegex: `/python/`,
-          },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/stavis-dev',
